1c58f8560736e98903956b12932a00406e774ee6
Ralph Giles <giles@mozilla.com>
2016-01-25 16:33:04 -0800

    Setting the size explicitly works.
    It looks like get_contents() doesn't actually resolve the mark().

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     let size = Label::new();
     let section = Section::new()
         .B32(&size)
-        .append_bytes(b"test")
-        .mark(&size);
+        .append_bytes(b"test");
+    size.set_const(section.size());
+
     let result = section.get_contents();
     assert!(result.is_some());
+
     println!("{:?}", result.unwrap());
 }
